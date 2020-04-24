79f66cb90d9b8eafa368a4e461b65cf51c07c428
antur84 <andreas.turku@gmail.com>
2020-04-24 11:18:26 +0200

    to-arrow-syntax-example.ts test file updated.

--- src/test/suite/commands/to-arrow-syntax/to-arrow-syntax-example.ts
+++ src/test/suite/commands/to-arrow-syntax/to-arrow-syntax-example.ts
@@ -1,5 +1,19 @@
 class FuncToArrowExample {
-  helloWorld() {
-    return 'hello';
+  private complexMethodWithParamsAndArgs<T>(t: T) {
+    return t;
   }
+  simpleMethod() {
+    const a = 1 + 1;
+    return a;
+  }
+
+  arrowProperty = () => {};
+
+  methodInClass() {
+    this.complexMethodWithParamsAndArgs(10);
+  }
+}
+function funcDeclaration() {
+  var test = new FuncToArrowExample();
+  test.simpleMethod();
 }
