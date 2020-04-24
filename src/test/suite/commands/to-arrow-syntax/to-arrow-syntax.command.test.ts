ca30a364579ee4e74180a8b5fba69bc1dc1848de
antur84 <andreas.turku@gmail.com>
2020-04-24 11:51:10 +0200

    use mocha bdd
    I'm more familiar with describe and it

--- src/test/suite/commands/to-arrow-syntax/to-arrow-syntax.command.test.ts
+++ src/test/suite/commands/to-arrow-syntax/to-arrow-syntax.command.test.ts
@@ -2,18 +2,22 @@ import * as path from 'path';
 import * as vscode from 'vscode';
 import { toArrowSyntaxCommand } from '../../../../commands/to-arrow-syntax/to-arrow-syntax.command';
 import { sleep } from '../../../utils/sleep';
-suite('Func to Arrow Test Suite', () => {
-  test('Can read test file', async () => {
-    const uri = vscode.Uri.file(
-      path
-        .join(__dirname + '/../to-arrow-syntax/to-arrow-syntax-example.ts')
-        .replace(/\\out\\/, '\\src\\')
-    );
-    const document = await vscode.workspace.openTextDocument(uri);
-    const editor = await vscode.window.showTextDocument(document);
-    editor.selection = new vscode.Selection(1, 2, 1, 12);
+describe('to-arrow-syntax Test Suite', () => {
+  it('should convert function declarations', async () => {
+    const { editor } = await loadEditor();
+    editor.selection = new vscode.Selection(15, 20, 15, 21);
     vscode.commands.executeCommand(toArrowSyntaxCommand.name);
-
     return sleep(60000);
   });
 });
+
+const loadEditor = async () => {
+  const uri = vscode.Uri.file(
+    path
+      .join(__dirname + '/../to-arrow-syntax/to-arrow-syntax-example.ts')
+      .replace(/\\out\\/, '\\src\\')
+  );
+  const document = await vscode.workspace.openTextDocument(uri);
+  const editor = await vscode.window.showTextDocument(document);
+  return { document, editor };
+};
--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 export function run(): Promise<void> {
   // Create the mocha test
   const mocha = new Mocha({
-    ui: 'tdd',
+    ui: 'bdd',
     timeout: 600000
   });
   mocha.useColors(true);
